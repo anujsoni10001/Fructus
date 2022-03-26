a4a3c307788fe7bc2f13aa99f8d276f4fbe8591a
Anuj Soni <anujsoni10001@gmail.com>
2022-03-26 11:10:21 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Anuj Soni on 26/03/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
